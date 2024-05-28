0ec32db6e0c475fd123599eb35550376a251a44f
Maycon Arruda <maycon.unix@gmail.com>
2024-05-28 10:31:55 -0300

    Aprendendo a usar Fuse.js

--- help.js
+++ help.js
@@ -4,19 +4,45 @@ console.log(teste);
 let help ;
 
 
-
-
+const { exec } = require('node:child_process');
+exec('clear')
 const tetra = (a)=>{
     console.log(a)
     // return a == undefined?  "ude" : "undeffined a"
     // console.log()
 }
 
-console.log(tetra());
+// console.log(tetra());
 // console.log(help == undefined? false: true )
 
 
+const removeAccents = str =>
+    str.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+  
+  removeAccents('Antoine de Saint-Exupéry'); // 'Antoine de Saint-Exupery'
+console.log(removeAccents('Antoine Japão'));
 
+// esssa parte é para tratar e buscar usando comparação, a natureza jurica do cnpj
+
+const axios = require('axios')
+const Fuse = require('fuse.js')
+
+// axios.get('https://api.cnpjs.dev/v1/00394684000153').then((response) =>{
+//   let {data} = response
+//   let {endereco, natureza_juridica,razao_social} = data
+//   let {uf} = endereco
+//   console.log({uf,natureza_juridica, razao_social});
+// })
+
+let list= ["Old Man's War", "The Lock Artist",'Municipio']
+const options = {
+  includeScore: true
+}
+
+const fuse = new Fuse(list)
+
+const result = fuse.search('cipio')
+console.log(result);
 
 // const reg = /\d{3}/g;
 // const str = "Java323Scr995ip4894545t";
